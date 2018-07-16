e6aa8e3b7f924d30abe1c2db770e45a02d8f775a
Tomasz Dysinski <tom@wgames.com>
2018-07-16 10:31:16 -0400

    lit sprite test with normals

---
+++ Assets/Spin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Spin : MonoBehaviour {
+
+	[Range(0f, 50f)]
+	public float speed = 5f;
+	// Use this for initialization
+	void OnEnable () {
+		EditorApplication.update += Update;
+	}
+	void OnDisable () {
+		EditorApplication.update -= Update;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0f, 0f, (float) EditorApplication.timeSinceStartup * speed);
+	}
+}
